3d0861819d40c8a62da32d13c95624e2f6de74df
Baptiste Langlade <baptouuuu@gmail.com>
2023-11-08 14:43:01 +0100

    add Instance::of()

---
+++ proofs/instance.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types = 1);
+
+use Innmind\Validation\Instance;
+use Innmind\BlackBox\Set;
+
+return static function() {
+    yield proof(
+        'Instance::of()',
+        given(Set\Type::any()),
+        static function($assert, $other) {
+            $std = new \stdClass;
+            $assert->true(
+                Instance::of(\stdClass::class)->asPredicate()($std),
+            );
+            $assert->same(
+                $std,
+                Instance::of(\stdClass::class)($std)->match(
+                    static fn($value) => $value,
+                    static fn() => null,
+                ),
+            );
+            $assert->false(
+                Instance::of(\stdClass::class)->asPredicate()($other),
+            );
+            $assert->same(
+                [['$', 'Value is not an instance of stdClass']],
+                Instance::of(\stdClass::class)($other)->match(
+                    static fn() => null,
+                    static fn($failures) => $failures
+                        ->map(static fn($failure) => [
+                            $failure->path()->toString(),
+                            $failure->message(),
+                        ])
+                        ->toList(),
+                ),
+            );
+        },
+    );
+};
---
+++ src/Instance.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Validation;
+
+use Innmind\Immutable\{
+    Validation,
+    Predicate,
+};
+
+/**
+ * @template-covariant T of object
+ * @implements Constraint<mixed, T>
+ * @psalm-immutable
+ */
+final class Instance implements Constraint
+{
+    /** @var Predicate\Instance<T> */
+    private Predicate\Instance $assert;
+    /** @var class-string<T> */
+    private string $class;
+
+    /**
+     * @param Predicate\Instance<T> $assert
+     * @param class-string<T> $class
+     */
+    private function __construct(Predicate\Instance $assert, string $class)
+    {
+        $this->assert = $assert;
+        $this->class = $class;
+    }
+
+    public function __invoke(mixed $value): Validation
+    {
+        /** @var Validation<Failure, T> */
+        return match (($this->assert)($value)) {
+            true => Validation::success($value),
+            false => Validation::fail(Failure::of("Value is not an instance of {$this->class}")),
+        };
+    }
+
+    /**
+     * @template A of object
+     * @psalm-pure
+     *
+     * @param class-string<A> $class
+     *
+     * @return self<A>
+     */
+    public static function of(string $class): self
+    {
+        return new self(Predicate\Instance::of($class), $class);
+    }
+
+    public function and(Constraint $constraint): Constraint
+    {
+        return AndConstraint::of($this, $constraint);
+    }
+
+    public function asPredicate(): Predicate
+    {
+        return $this->assert;
+    }
+}
